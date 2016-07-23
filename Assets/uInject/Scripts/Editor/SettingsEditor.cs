0d5a2f21eca9e9d340bc44ab9f608fe283f4badd
Marco Knietzsch <marco.knietzsch@rezahas.com>
2016-07-23 19:12:31 +0900

    added export package and shortcut to options

--- Assets/uInject/Scripts/Editor/SettingsEditor.cs
+++ Assets/uInject/Scripts/Editor/SettingsEditor.cs
@@ -7,11 +7,42 @@ namespace Ninject.Unity.Editor
 	[CustomEditor(typeof(Settings))]
 	public class SettingsEditor : UnityEditor.Editor
 	{
+		private const string SETTINGS_NAME = "uInject Options";
+		private static Settings instance;
 		private string newModuleName = "New Module";
+
+		private static Settings Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = (Settings)Resources.Load(SETTINGS_NAME);
+				}
+				return instance;
+			}
+		}
+
+		[MenuItem("Window/uInject/Options")]
+		public static void Edit()
+		{
+			UnityEditor.Selection.activeObject = Instance;
+		}
 
 		public override void OnInspectorGUI()
 		{
 			DrawNewModule();
+			EditorGUILayout.Separator();
+			DrawPackage();
+		}
+
+		private static void DrawPackage()
+		{
+			if (GUILayout.Button("Export Unity Package"))
+			{
+				ExportPackageOptions options = ExportPackageOptions.Interactive | ExportPackageOptions.Recurse;
+				AssetDatabase.ExportPackage("Assets/uInject", "uInject.unitypackage", options);
+			}
 		}
 
 		private void DrawNewModule()
